cecbaeabe7c16f6c165b355b00cf6e4bda6dc7b7
fenetre42 <william.desperques@gmail.com>
2021-11-25 18:15:25 +0100

    true

---
+++ atelier.sh
@@ -0,0 +1,53 @@
+clear
+cat ./Ressources/Déroulé.txt
+read a
+clear
+cat ./Ressources/Partie1
+read a
+cat ./Ressources/Blank2
+cat ./Ressources/Consigne.txt
+cat ./Ressources/Exercice_1.txt
+cat ./Ressources/Intercalaire
+read a
+cat ./Ressources/Réponse_1.txt
+cat ./Ressources/Intercalaire
+read a
+clear
+cat ./Ressources/Partie1
+cat ./Ressources/Blank2
+cat ./Ressources/Consigne.txt
+cat ./Ressources/Exercice_2.txt
+cat ./Ressources/Intercalaire
+read a
+cat ./Ressources/Blank2
+cat ./Ressources/Réponse_2.txt
+cat ./Ressources/Intercalaire
+read a
+clear
+cat ./Ressources/Partie1
+cat ./Ressources/Blank2
+cat ./Ressources/Consigne.txt
+cat ./Ressources/Exercice_3.txt
+cat ./Ressources/Intercalaire
+read a
+cat ./Ressources/Réponse_3.txt
+cat ./Ressources/Intercalaire
+read a
+clear
+cat ./Ressources/Partie2
+read a
+cat ./Ressources/Blank2
+cat ./Ressources/Commandes_intro.txt
+read a
+clear
+bash ./Ressources/fonctions.sh
+read a
+clear
+cat ./Ressources/Partie3
+read a
+cat ./Ressources/Blank2
+cat ./Ressources/Rédaction.txt
+read a
+clear
+bash ./Ressources/fonctions_2.sh
+read a
